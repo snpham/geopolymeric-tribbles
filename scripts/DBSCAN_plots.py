8f544c7ff914fddccd307192a9a1c4ac48a7113a
reiko-elena <70383883+reiko-elena@users.noreply.github.com>
2021-08-09 20:19:27 -0600

    DBSCAN plots and data

---
+++ scripts/DBSCAN_plots.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import os
+import plotly.express as px
+
+cwd = os.getcwd()
+
+info_results = pd.read_feather(cwd + "\\cluster_results\\info.feather")
+
+fig = px.scatter(info_results, x="SocialmediatrustQ1", y="CanadaQ_1", color="labels",
+                 size='WHOtrustQ1', labels=dict(CanadaQ_1="Susceptibility to Misinformation (1 is most susceptible)",
+                                                SocialmediatrustQ1="Trust in social media (7 is complete trust)"),
+                 title='<b>Results of DBSCAN clustering based on trust in information sources</b><br>\
+                        Epsilon = 1, Attributes : Canada_Q1, SocialmediatrustQ1,journalisttrustQ1,FriendstrustQ1,WHOtrustQ1<br>\
+                        ')
+fig.show()
+
+politics_results = pd.read_feather(cwd + "\\cluster_results\\politics.feather")
+
+fig = px.scatter(politics_results, x="Politics", y="labels", color="labels",
+                 labels=dict(labels="Assigned cluster label",
+                             Politics="Politics (1 very left, 7 very right)"),
+                 title='<b>Results of DBSCAN clustering based on trust in information sources</b><br>\
+                        Epsilon = 1, Attributes :CanadaQ_1, Politics, prosocial, CultCog_1, CultCog_2,CultCog_3<br>\
+                        Susceptible individuals are in clusters 2 and 7, all others are less susceptible')
+fig.show()
