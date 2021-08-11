f766eda31096217fb986f4314089a505f38fa4d8
RyanKaras <59896887+RyanKaras@users.noreply.github.com>
2021-08-11 14:26:51 -0400

    Attribute Count Graph Function

---
+++ scripts/RyanSumCharts.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import random
+import sklearn.cluster as skl
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.io as pio
+
+firstDF = DF("integrated_dataNew.csv")
+
+def scrubDF(df, keepList):
+    df = df.drop(index=[0, 1, 2, 3])
+    for col in df.columns:
+        if col not in keepList:
+            df = df.drop(col, axis=1)
+
+    df1 = df.dropna()
+    return df1
+
+def graphCount(uncleandf, xAxis, yAxis, xName, yName, graphTitle):
+    df = scrubDF(uncleandf, [xAxis,yAxis])
+    s1 = df.groupby([xAxis,yAxis]).size()
+    sumData = []
+    for con, pol in zip(s1.index,s1):
+        sumData.append([con[0], con[1], pol])
+    newDF = pd.DataFrame(np.array(sumData), columns=[xName, yName, "Count"])
+    newDF["Count"] = pd.to_numeric(newDF["Count"])
+
+    fig = px.bar(newDF, x = xName, y = "Count", color=yName, title = graphTitle)
+    fig.show()
+    return newDF
+       
+plot1 = graphCount(firstDF, 'Residency', 'Politics', "Country", "Political Leaning (1-Conservative, 7-Liberal)", "Political Leaning by Country")
+plot2 = graphCount(firstDF, 'Politics', 'Residency', "Political Leaning (1-Conservative, 7-Liberal)", "Country", "Political Leaning Breakdown")
